981a3abac4e6ae2630223c872a955832ccdb6b89
AlexCavalcantiJordao <alex37705732@gmail.com>
2024-07-31 18:00:19 -0300

    'Modelo SQL.mwb'   db_Biblioteca.sql

--- db_Biblioteca.sql
+++ db_Biblioteca.sql
@@ -56,12 +56,34 @@ references tbl_editoras(ID_editora);
 select * from tbl_Livro;
 
 
-insert into tbl_autores(ID_Autor, Nome_Autor, SobreNome_Autor) values(1, "Daniel", "Barret");
+insert into tbl_autores(ID_Autor, Nome_Autor, SobreNome_Autor) values(1, 'Daniel', 'Barret');
 
-insert into tbl_autores(ID_Autor, Nome_Autor, SobreNome_Autor) values(2, "Gerald", "Carter");
+insert into tbl_autores(ID_Autor, Nome_Autor, SobreNome_Autor) values(2, 'erald', 'Carter');
 
-insert into tbl_autores(ID_Autor, Nome_Autor, SobreNome_Autor) values(3, "Mark", "Sobell");
+insert into tbl_autores(ID_Autor, Nome_Autor, SobreNome_Autor) values(3, 'Mark', 'Sobell');
 
-insert into tbl_autores(ID_Autor, Nome_Autor, SobreNome_Autor) values(4, "William", "Stanek");
+insert into tbl_autores(ID_Autor, Nome_Autor, SobreNome_Autor) values(4, 'William', 'Stanek');
 
-insert into tbl_autores(ID_Autor, Nome_Autor, SobreNome_Autor) values(5, "Richard", "Blum");
+insert into tbl_autores(ID_Autor, Nome_Autor, SobreNome_Autor) values(5, 'Richard', 'Blum');
+
+select * from tbl_autores;
+
+INSERT INTO tbl_editoras (Nome_Editora) VALUES ('Prentice Hall');
+
+INSERT INTO tbl_editoras (Nome_Editora) VALUES ('O´Reilly');
+
+INSERT INTO tbl_editoras (Nome_Editora) VALUES ('Microsoft Press');
+
+INSERT INTO tbl_editoras (Nome_Editora) VALUES ('Wiley');
+
+INSERT INTO tbl_editoras (Nome_Editora) VALUES ('McGraw-Hill Education');
+
+INSERT INTO tbl_Livro (Nome_Livro, ISBN13, ISBN10, Data_Pub, Preco_Livro, Categoria, ID_Autor, ID_Editora)
+VALUES
+('Linux Command Line and Shell Scripting','9781118983843', '111898384X', '20150109', 68.35, 1, 5, 4),
+('SSH, the Secure Shell','9780596008956', '0596008953', '20050517', 58.30, 1, 1, 2),
+('Using Samba','9780596002565', '0596002564', '20031221', 61.45, 1, 2, 2),
+('Fedora and Red Hat Linux', '9780133477436', '0133477436', '20140110', 62.24, 1, 3, 1),
+('Windows Server 2012 Inside Out','9780735666313', '0735666318', '20130125', 66.80, 1, 4, 3),
+('Microsoft Exchange Server 2010','9780735640610', '0735640610', '20101201', 45.30, 1, 4, 3),
+('Practical Electronics for Inventors', '9781259587542', '1259587541', '20160324', 67.80, 1, 13, 5);
